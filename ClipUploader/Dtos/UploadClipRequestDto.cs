4908c3fc95f471b6bf74b04f6a6f3529e1b90ba6
Kevin Bongcasan <kbongcasmis@gmail.com>
2023-09-07 00:04:48 -0500

    Added Upload validation

--- ClipUploader/Dtos/UploadClipRequestDto.cs
+++ ClipUploader/Dtos/UploadClipRequestDto.cs
@@ -7,7 +7,7 @@ public class UploadClipRequestDto
     [Required, StringLength(60, MinimumLength = 1)]
     public string Name { get; set; } 
 
-    [StringLength(60)]
+    [StringLength(280)]
     public string? Description { get; set; }
 
     public bool Public { get; set; }
